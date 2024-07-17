cd5f8879559442fae0557ab44214c3a4be7dcde3
MA7EJA <matejajovanovic63@gmail.com>
2024-07-17 17:44:57 +0200

    feat: Add loginUser functionality

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -44,7 +44,19 @@ const registerUser = tryCatch(async (req, res) => {
 })
 
 const loginUser = tryCatch(async (req, res) => {
-    
+    const {email, password} = req.body
+
+    const user = await User.findOne({email});
+
+    if(!user) return res.status(404).json({ msg: 'User not exist'});
+
+    const comparePassword = await bcrypt.compare(password, user.password);
+
+    if(!comparePassword) return res.status(400).json({ msg: "Invalid Password"});
+
+    generateToken(user._id, res)
+
+    res.json({ msg: "User Logged In", user})
 })
 
 module.exports = { registerUser, loginUser };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { registerUser } = require("../controllers/authControllers");
+const { registerUser, loginUser } = require("../controllers/authControllers");
 const uploadFile = require("../middlewares/multer");
 
 const router = express.Router();
 
 router.post("/register", uploadFile ,registerUser);
+router.post("/login", loginUser)
 
 module.exports = router;
