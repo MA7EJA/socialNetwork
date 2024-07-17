b2d378f8b3dfa6b71269431565b0db2f1bcbb378
MA7EJA <matejajovanovic63@gmail.com>
2024-07-17 12:28:29 +0200

    feat: Add User Model

---
+++ server/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["male", "female"],
+  },
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  followings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  profilePicture: {
+    id: String,
+    url: String
+  }
+}, 
+{
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema)
