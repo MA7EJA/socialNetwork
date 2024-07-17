e88ee0c3039ac32224e1f9496a4ed8cb1d0286a4
MA7EJA <matejajovanovic63@gmail.com>
2024-07-17 12:45:10 +0200

    feat: Add User Route

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const connectToDb = require('./dataBase/db');
 require('dotenv').config();
 
+const router = require('./routes/Routes')
+
 const app = express()
+
+app.use('/api', router)
 
 app.listen(process.env.PORT, () => {
         if (!process.env.PORT) {
