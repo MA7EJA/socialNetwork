87b47b172f8a009576c02f14da1cba424f93a711
MA7EJA <matejajovanovic63@gmail.com>
2024-07-18 14:29:06 +0200

    feat: Added Delete Post Functionality to postControllers.js

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -28,6 +28,20 @@ const newPost = tryCatch(async (req, res) => {
     const post = await Post.create({ caption, post: { id: myCloud.public_id, url: myCloud.secure_url }, owner: ownerId, type});
 
     res.status(201).json({ msg: "Post Created", post})
+});
+
+const deletePost = tryCatch(async (req, res) => {
+    const post = await Post.findById(req.params.id)
+
+    if(!post) return res.status(404).json({ msg: 'No Post with this ID'});
+
+    if(post.owner.toString() !== req.user._id.toString()) return res.status(403).json({ msg: "Unauthorized"});
+
+    await cloudinary.v2.uploader.destroy(post.post.id);
+
+    await post.deleteOne();
+
+    res.json({ msg: "Post Deleted"})
 })
 
-module.exports = {newPost}
+module.exports = { newPost, deletePost };
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const isAuth = require("../middlewares/isAuth");
-const { newPost } = require("../controllers/postControllers");
+const { newPost, deletePost } = require("../controllers/postControllers");
 const uploadFile = require("../middlewares/multer");
 
 const router = express.Router();
 
 router.post('/new', isAuth, uploadFile ,newPost);
+router.delete("/:id", isAuth, deletePost);
 
 module.exports = router
