27c2ea642bb89e9705ccc0ae6e6cdb7c977d10b7
MA7EJA <matejajovanovic63@gmail.com>
2024-07-18 19:23:45 +0200

    feat: Add messagesModel Schema

---
+++ server/models/messagesModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  chatId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Chat",
+  },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  text: String,
+},{
+    timestamps: true
+});
+
+module.exports = mongoose.model('Messages', messageSchema)
