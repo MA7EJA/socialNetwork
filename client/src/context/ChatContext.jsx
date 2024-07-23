2fa5626aeef8260fe2e52dfd4d51700a191b4191
MA7EJA <matejajovanovic63@gmail.com>
2024-07-23 12:06:27 +0200

    feat: add ChatContext for chat state management

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,10 +1,36 @@
 import axios from "axios";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import toast from "react-hot-toast";
 
 const ChatContext = createContext()
 
 export const ChatContextProvider = ({ children }) => {
-    return <ChatContext.Provider value={{}}>{children}</ChatContext.Provider>
+
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const darkStyle = {
+      background: 'rgb(31 41 55)',
+      color: '#fff',
+    };
+
+    const [chats, setChats] = useState([])
+    const [selectedChat, setSelectedChat] = useState(null)
+
+    async function createChat(id){
+        const toastId = toast.loading("Processing...", { style: isDarkMode ? darkStyle : {} });
+        try {
+            const { data } = await axios.post('/api/message', {
+                reciverId: id,
+                message: "hii"
+            })
+            
+            toast.success(data.msg, { style: isDarkMode ? darkStyle : {}, id: toastId });
+
+        } catch (err) {
+            toast.error(err.response.data.msg, { style: isDarkMode ? darkStyle : {}, id: toastId })
+        }
+    }
+
+    return <ChatContext.Provider value={{ createChat, selectedChat, setSelectedChat, chats, setChats }}>{children}</ChatContext.Provider>
 }
 
 export const ChatData = () => useContext(ChatContext)
