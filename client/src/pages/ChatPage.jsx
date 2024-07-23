19a9425f41934e37dcd5939a9a11d44ba33ca0a8
MA7EJA <matejajovanovic63@gmail.com>
2024-07-23 12:06:51 +0200

    feat: add ChatPage component with user search functionality

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ChatData } from '../context/ChatContext'
+import axios from 'axios'
 
 const ChatPage = () => {
+
+    const { createChat, selectedChat, setSelectedChat, chats, setChats } = ChatData()
+
+    const [users, setUsers] = useState([])
+    const [query, setQuery] = useState('')
+    const [search, setSearch] = useState(false)
+
+    async function fetchAllUsers(){
+        try {
+            const { data } = await axios.get('/api/user/all?search=' + query)
+
+            setUsers(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchAllUsers()
+    }, [query])
+
   return (
     <div>ChatPage</div>
   )
